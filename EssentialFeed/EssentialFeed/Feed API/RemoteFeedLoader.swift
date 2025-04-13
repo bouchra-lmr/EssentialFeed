023436577ce04495bbcd9542b43e40b9a759cfb1
Bouchra Bekkouche <bouchra.llmr@gmail.com>
2025-04-13 22:49:29 +0200

    Delivers invalidData error on non 200 HTTP responses

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -11,7 +11,7 @@ public protocol HTTPClient {
             
     func get(
         from url: URL,
-        completion: @escaping (Error) -> Void
+        completion: @escaping (Error?, HTTPURLResponse?) -> Void
     )
 }
 
@@ -23,6 +23,7 @@ public class RemoteFeedLoader {
     
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
     
     public init(client: HTTPClient, url: URL) {
@@ -31,8 +32,13 @@ public class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error in
-            completion(.connectivity)
+        client.get(from: url) { error, response in
+            
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
         }
     }
 }
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -13,7 +13,7 @@ class RemoteFeeLoaderTests: XCTest {
     func test_init_doesNotRequestDataFromURL() {
         
         let (_, client) = makeSUT()
-                         
+        
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
@@ -43,7 +43,7 @@ class RemoteFeeLoaderTests: XCTest {
     func test_load_deliversErrorOnClientError() {
         
         let (sut, client) = makeSUT()
-
+        
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
         
@@ -51,6 +51,18 @@ class RemoteFeeLoaderTests: XCTest {
         client.complete(with: clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
+    }
+    
+    func test_load_deliversErrorOnNon200Error() {
+        
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        client.complete(withStatusCode: 400)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
     }
     
     // MARK: - Helpers
@@ -68,20 +80,32 @@ class RemoteFeeLoaderTests: XCTest {
         var requestedURLs: [URL] {
             messages.map { $0.url }
         }
-                
-        var messages = [(url: URL, completion: (Error) -> Void)]()
+        
+        var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
         
         func get(
             from url: URL,
-            completion: @escaping (Error) -> Void
+            completion: @escaping (Error?, HTTPURLResponse?) -> Void
         ) {
-                                   
+            
             messages.append((url, completion))
             
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
+        }
+        
+        func complete(withStatusCode statusCode: Int, at index: Int = 0) {
+            
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: statusCode,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            
+            messages[index].completion(nil, response)
         }
     }
 }
